0d0b7c871437fd069b1e1670a0d72ff9258f9d05
jeff283 <njorogejeff2019@gmail.com>
2024-12-19 16:45:00 +0300

    Added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
 }
